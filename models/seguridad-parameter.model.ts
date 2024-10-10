c889f0d93c22b5c8bfbf18ba1b73ea673f29431c
develHector <devel.hector@hotmail.com>
2024-10-09 19:49:33 -0600

    Removed the 'id' parameter as it's unused in the interaction with the frontend

--- models/seguridad-parameter.model.ts
+++ models/seguridad-parameter.model.ts
@@ -1,6 +1,6 @@
 // Define an interface for the parameters table
+// id: number; // we can remove the user id from the model because it is not needed in the form, nor the interaction with the controller endpoints (as the id comes in the token)
 export interface SeguridadParameterModel {
-    id: number;
     application: string;
     parameter: string;
     value?: string;
